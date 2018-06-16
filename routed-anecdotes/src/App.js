89dc3c76e057aa18c4e0245bea49ab1c8b035171
Henrik Lyra <henrik.lyra@stat.fi>
2018-06-16 19:37:30 +0300

    tehtävät 6.21 - 6.23

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,54 +1,85 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import {
+  ListGroup, ListGroupItem, Image, Grid, Row, Col, Alert, Jumbotron, FormGroup,
+  ControlLabel, FormControl, Button, Navbar, Nav, NavItem
+} from 'react-bootstrap'
 
 const Menu = () => (
-  <div style={menuStyle}>    
-    <NavLink  activeStyle={{fontWeight: 'bold'}} exact to='/'>anecdotes</NavLink>&nbsp;
-    <NavLink activeStyle={{fontWeight: 'bold'}} exact to='/create'>create new</NavLink>&nbsp;
-    <NavLink activeStyle={{fontWeight: 'bold'}} exact to='/about'>about</NavLink>&nbsp;
-  </div>
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        Anecdote app
+    </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem href="#">
+          <Link to="/">anecdotes</Link>
+        </NavItem>
+        <NavItem href="#">
+          <Link to="/create">create new</Link>
+        </NavItem>
+        <NavItem href="#">
+          <Link to="/about">about</Link>
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
-    <ul>
+    <ListGroup>
       {anecdotes.map(anecdote =>
-        <li key={anecdote.id} >
+        <ListGroupItem key={anecdote.id} >
           <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>
+        </ListGroupItem>
       )}
-    </ul>  
+    </ListGroup>
   </div>
 )
 
 const Anecdote = ({ anecdote }) => (
-  <div>
-        <h2>{anecdote.content} by {anecdote.author}</h2>
-        <div>has {anecdote.votes} votes</div>
-        <div>for more see <a>{anecdote.info}</a></div>
-  </div>
+  <Jumbotron>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <div>has {anecdote.votes} votes</div>
+    <div>for more see <a>{anecdote.info}</a></div>
+  </Jumbotron>
 )
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+  <Grid>
+    <Row>
+      <Col md={8}>
+        <h2>About anecdote app</h2>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={8}>
+        <p>According to Wikipedia:</p>
+
+        <em>An anecdote is a brief, revealing account of an individual person or an incident.
+          Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+          such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
       An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
+        <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      </Col>
+      <Col>
+        <Image src="https://myhero.com/images/guest/g27620/hero25988/g27620_u25887_chimage.jpeg" thumbnail />
+      </Col>
+    </Row>
+  </Grid>
 )
 
 const Footer = () => (
   <div>
     Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
 
-    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
+    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code.
   </div>
 )
 
@@ -79,25 +110,27 @@ class CreateNew extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <div>
         <h2>create a new anecdote</h2>
         <form onSubmit={this.handleSubmit}>
-          <div>
-            content 
-            <input name='content' value={this.state.content} onChange={this.handleChange} />
-          </div>
-          <div>
-            author
-            <input name='author' value={this.state.author} onChange={this.handleChange} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={this.state.info} onChange={this.handleChange} />
-          </div> 
-          <button>create</button>
+          <FormGroup>
+            <ControlLabel>content</ControlLabel>
+            <FormControl name='content' value={this.state.content} onChange={this.handleChange} />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>author</ControlLabel>
+            <FormControl name='author' value={this.state.author} onChange={this.handleChange} />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>url for more info</ControlLabel>
+            <FormControl name='info' value={this.state.info} onChange={this.handleChange} />
+          </FormGroup>
+          <FormGroup>
+            <Button type="submit" bsStyle="success">create</Button>
+          </FormGroup>
         </form>
-      </div>  
+      </div>
     )
 
   }
@@ -126,14 +159,14 @@ class App extends React.Component {
         }
       ],
       notification: ''
-    } 
+    }
   }
 
   addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
     this.setState({ anecdotes: this.state.anecdotes.concat(anecdote) })
-    this.setState({ notification: `a new ancdote ${anecdote.content} created!`})
-    setTimeout(() => this.setState({notification: ''}), 10000)
+    this.setState({ notification: `a new ancdote ${anecdote.content} created!` })
+    setTimeout(() => this.setState({ notification: '' }), 10000)
   }
 
   anecdoteById = (id) =>
@@ -152,24 +185,22 @@ class App extends React.Component {
     this.setState({ anecdotes })
   }
 
-  
+
 
   render() {
-    const notificationVisibility = { display: this.state.notification === '' ? 'none' : '' }
-    console.log(this.state.notification)
     return (
-      <div>
+      <div className="container">
         <Router>
           <div>
             <h1>Software anecdotes</h1>
             <Menu />
-            <div style={notificationVisibility}>
-              <div style={notificationStyle}>{this.state.notification}</div>
-            </div>
+            {(this.state.notification &&
+              <Alert color="succes">{this.state.notification}</Alert>
+            )}            
             <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route path="/about" render={() => <About />} />
-            <Route path="/create" render={({history}) => <CreateNew history={history} addNew={this.addNew} />} />
-            <Route exact path="/anecdotes/:id" render={({match}) =>
+            <Route path="/create" render={({ history }) => <CreateNew history={history} addNew={this.addNew} />} />
+            <Route exact path="/anecdotes/:id" render={({ match }) =>
               <Anecdote anecdote={this.anecdoteById(match.params.id)} />}
             />
             <Footer />
@@ -180,20 +211,20 @@ class App extends React.Component {
   }
 }
 
-const notificationStyle = {
-  color: 'green',
-  fontSize: 16,
-  borderStyle: 'solid',
-  borderWidth: 1,
-  borderRadius: 5,
-  padding: 10,
-  margin: 5
-}
+// const notificationStyle = {
+//   color: 'green',
+//   fontSize: 16,
+//   borderStyle: 'solid',
+//   borderWidth: 1,
+//   borderRadius: 5,
+//   padding: 10,
+//   margin: 5
+// }
 
-const menuStyle = {
-  backgroundColor: 'lightblue',
-  padding: 10,
-  margin: 5
-}
+// const menuStyle = {
+//   backgroundColor: 'lightblue',
+//   padding: 10,
+//   margin: 5
+// }
 
 export default App;
